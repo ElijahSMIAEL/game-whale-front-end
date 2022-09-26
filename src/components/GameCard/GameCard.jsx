3560253a1cdfeae9f8b42cf078f28b96bdf4424a
CoolRy <that007guy@hotmail.com>
2022-09-26 15:48:54 -0400

    api service stubb up

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,10 +1,14 @@
 import styles from './GameCard.module.css'
 import GameDetails from '../../pages/GameDetails/GameDetails'
+import { Link } from 'react-router-dom'
 
-const GameCard = () => {
+const GameCard = (props) => {
   return (
     <>
     <h1>GameCardComponent</h1>
+    <Link to={`/game/${props.game.name}`}>
+      {props.game.name}
+    </Link>
     <GameDetails />
     </>
   )
