635c28bbd4cd277ba584ead74ec87d61d8182343
CoolRy <that007guy@hotmail.com>
2022-09-26 15:13:09 -0400

    add search bar

--- src/pages/Games/Games.jsx
+++ src/pages/Games/Games.jsx
@@ -1,13 +1,34 @@
 import styles from './Games.module.css'
+import { useState } from 'react'
 import GameCard from '../../components/GameCard/GameCard'
+import SearchForm from '../../components/SearchForm/SearchForm'
+
 
 const Games = () => {
-  return (
-    <div>
-    <h1>GamesComponent</h1>
-    <GameCard />
-    </div>
-  )
-}
+  const [games,setGames] = useState([])
 
-export default Games
+  const handleGameSearch = async formData => {
+    const gameResults = await handleGameSearch(formData)
+    setGames(gameResults.results)
+  }
+  return (
+    <>
+      <h3>Game Search</h3>
+      <SearchForm handleGameSearch={handleGameSearch} />
+      {games.length ? 
+        <>
+          {games.map(game => 
+            <GameCard 
+              game={game}
+              key={game.index}
+            />
+          )}
+        </>
+        :
+        <h3>Please search for a Game!</h3>
+      }
+    </>
+  );
+}
+ 
+export default Games;
